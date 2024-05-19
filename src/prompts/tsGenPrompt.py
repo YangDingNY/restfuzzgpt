a6fe6f8227052607e2ba4baec324d3f3cc55da1c
Yang Ding <191250174@smail.nju.edu.cn>
2024-05-19 12:39:45 +0800

    format json

--- src/prompts/tsGenPrompt.py
+++ src/prompts/tsGenPrompt.py
@@ -3,6 +3,7 @@ def getMessage():
     Now you need to generate a request sequence for a test case.
     You should return me a string that includes multiple requests with context, each request being represented by the operationId in the Swagger/OpenAPI document it involves, separated by "->".
     Here is an example(The response should not contain any other content except for the following format):
-    operationA -> operationB -> operationC  
+    operationA -> operationB -> operationC
+    The operationA, operationB, and operationC here are just some substitutions. Please use the specific operationId in the swagger/openAPI document instead.  
     '''
     return tsGenPrompt
--- src/sendRequests.py
+++ src/sendRequests.py
@@ -2,14 +2,20 @@ import requests
 import json
 
 
-def send(request_json):
+def send(request_json, configFilePath=None):
     dict_json = json.loads(request_json)
+    url = dict_json["url"]
+    if configFilePath is not None:
+        configFile = open(configFilePath, 'r', encoding='utf-8')
+        configData = configFile.read()
+        configFile.close()
+        config = yaml.load(configData, Loader=yaml.FullLoader)
     if 'params' in dict_json:
-        response = requests.request(method=dict_json["http_method"], url=dict_json["url"], params=dict_json["params"])
+        response = requests.request(method=dict_json["http_method"], url=url, params=dict_json["params"])
     elif 'form_data' in dict_json:
-        response = requests.request(method=dict_json["http_method"], url=dict_json["url"], data=dict_json["form_data"])
+        response = requests.request(method=dict_json["http_method"], url=url, data=dict_json["form_data"])
     elif 'json_data' in dict_json:
-        response = requests.request(method=dict_json["http_method"], url=dict_json["url"], data=dict_json["json_data"])
+        response = requests.request(method=dict_json["http_method"], url=url, data=dict_json["json_data"])
     else:
-        response = requests.request(method=dict_json["http_method"], url=dict_json["url"])
+        response = requests.request(method=dict_json["http_method"], url=url)
     return str(response.content,encoding='utf-8')
